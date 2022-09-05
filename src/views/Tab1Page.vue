2f5ef9ba6acfd7042d22c345bd9e19d439ceff3e
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2022-09-06 01:02:47 +0200

    update

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Todolist</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
@@ -11,19 +11,83 @@
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+      <ion-grid fixed size="sm">
+        <ion-row>
+          <ion-col size="2">ID</ion-col>
+          <ion-col size="8">Name</ion-col>
+          <ion-col size="2">Action</ion-col>
+        </ion-row>
+        <ion-row v-for="todo in state.todoList" :key="'todo-' + todo.id">
+          <ion-col size="2">{{ todo.id }}</ion-col>
+          <ion-col size="8">{{ todo.name }}</ion-col>
+          <ion-col size="2" @click="deleteTodo(todo.id)">
+            <ion-button color="danger">
+              X
+            </ion-button>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+        <ion-fab-button color="success" id="open-modal">
+          +
+        </ion-fab-button>
+      </ion-fab>
+      <ion-modal ref="modal" trigger="open-modal">
+        <ion-header>
+          <ion-toolbar>
+            <ion-title>New todo</ion-title>
+            <ion-buttons slot="end">
+              <ion-button :strong="true" @click="addTodo">Confirm</ion-button>
+            </ion-buttons>
+          </ion-toolbar>
+        </ion-header>
+        <ion-content class="ion-padding">
+          <ion-item>
+            <ion-label position="stacked">Enter your name</ion-label>
+            <ion-input v-model="state.newTodoName" type="text" placeholder="Your todo name"></ion-input>
+          </ion-item>
+        </ion-content>
+      </ion-modal>
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+<script lang="ts" setup>
+import {defineComponent, reactive, ref} from 'vue';
+import {IonContent, IonHeader, IonInput, IonLabel, IonModal, IonPage, IonTitle, IonToolbar} from '@ionic/vue';
 
-export default  defineComponent({
+defineComponent({
   name: 'Tab1Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: {IonHeader, IonToolbar, IonTitle, IonContent, IonPage}
 });
+
+
+const state = reactive({
+  id: 2,
+  todoList: [
+    {
+      id: 1,
+      name: 'Eat apple'
+    }
+  ],
+  newTodoName: ''
+})
+
+// const toggleModal = () => modal.value = !modal.value;
+
+const addTodo = () => {
+  if (state.newTodoName.length > 0) {
+    state.todoList.push({
+      id: state.id,
+      name: state.newTodoName
+    });
+  }
+  state.id++;
+  state.newTodoName = '';
+}
+
+const deleteTodo = (id: number) => {
+  state.todoList = state.todoList.filter(todo => id !== todo.id);
+}
+
 </script>
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -5,14 +5,14 @@
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
           <ion-icon :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+          <ion-label>Todo</ion-label>
         </ion-tab-button>
-          
+
         <ion-tab-button tab="tab2" href="/tabs/tab2">
           <ion-icon :icon="ellipse" />
           <ion-label>Tab 2</ion-label>
         </ion-tab-button>
-        
+
         <ion-tab-button tab="tab3" href="/tabs/tab3">
           <ion-icon :icon="square" />
           <ion-label>Tab 3</ion-label>
@@ -32,8 +32,8 @@ export default defineComponent({
   components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage, IonRouterOutlet },
   setup() {
     return {
-      ellipse, 
-      square, 
+      ellipse,
+      square,
       triangle,
     }
   }
